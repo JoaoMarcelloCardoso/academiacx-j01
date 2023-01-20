90b79364d8259a0c54d141d45633a2378727fc4c
Daniel Weckerle Alvarez Nava <122762255+danielweckerle@users.noreply.github.com>
2023-01-19 22:34:02 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println( 2 );
-        System.out.println( 1 );
+        int a = 1;
+        int b = 0;
+        int aux;
+        for(int i = 0; i < 30;i++){
+            System.out.println(a); // 1 1 2 3
+            aux = a;
+            a = a + b;
+            b = aux;
+        }
     }
 }
